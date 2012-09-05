c9f770cc17e60922ab0c80b28d4e343db5805298
Filippov Aleksey <sarum9in@gmail.com>
2012-09-05 15:30:35 +0400

    Build scripts were updated.
    yandex.contest.invoker.compat.jni build was implemented.

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 lib_prefix=yandex_contest_
-libs=(common system invoker invoker_compat_common)
-# invoker_compat_jni)
+libs=(common system invoker invoker_compat_common invoker_compat_jni)
+java_version=1.0.0-BUILD-SNAPSHOT
