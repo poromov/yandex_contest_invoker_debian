972888b9b826252e765652e5fccc039028fbfc2f
Filippov Aleksey <sarum9in@gmail.com>
2012-09-12 19:32:55 +0400

    Git repository was changed.

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 lib_prefix=yandex_contest_
 libs=(common system invoker invoker_compat_common invoker_compat_jni invoker_flowctl_pipectl)
+repo=git://github.com/sarum9in
 
 if [[ -f user-config.sh ]]
 then
