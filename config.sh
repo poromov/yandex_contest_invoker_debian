ee17cee6bb8710143623ce68b313a3d6bfe6ca0f
Filippov Aleksey <sarum9in@gmail.com>
2012-09-12 17:42:39 +0400

    New part: invoker_flowctl_pipectl.

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 lib_prefix=yandex_contest_
-libs=(common system invoker invoker_compat_common invoker_compat_jni)
+libs=(common system invoker invoker_compat_common invoker_compat_jni invoker_flowctl_pipectl)
 
 if [[ -f user-config.sh ]]
 then
