73efa267795ec3586c78a726b73bfa3c3a21ea00
Filippov Aleksey <sarum9in@gmail.com>
2012-09-13 20:35:27 +0400

    New component: invoker_flowctl_game.

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 lib_prefix=yandex_contest_
-libs=(common system invoker invoker_compat_common invoker_compat_jni invoker_flowctl_pipectl)
+libs=(common system invoker invoker_compat_common invoker_compat_jni invoker_flowctl_pipectl invoker_flowctl_game)
 repo=git://github.com/sarum9in
 
 if [[ -f user-config.sh ]]
