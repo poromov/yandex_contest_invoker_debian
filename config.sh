0b80c440f4b2a78bb398f5f8bfed825c8bd25c9a
Filippov Aleksey <sarum9in@gmail.com>
2012-09-14 16:36:30 +0400

    build script configuration refactoring.

--- config.sh
+++ config.sh
@@ -2,6 +2,9 @@ lib_prefix=yandex_contest_
 libs=(common system invoker invoker_compat_common invoker_compat_jni invoker_flowctl_pipectl invoker_flowctl_game)
 repo=git://github.com/sarum9in
 
+MFLAGS=()
+CMAKE_FLAGS=("-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.7" "-DCMAKE_C_COMPILER=/usr/bin/gcc-4.7")
+
 if [[ -f user-config.sh ]]
 then
     . ./user-config.sh
