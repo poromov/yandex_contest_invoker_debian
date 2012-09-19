0b9b45c7290e6cff9724b3cbf3f21c56c1accf3e
Filippov Aleksey <sarum9in@gmail.com>
2012-09-19 16:46:58 +0400

    invoker_compat_jni_version integration.

--- config.sh
+++ config.sh
@@ -2,6 +2,8 @@ lib_prefix=yandex_contest_
 libs=(common system invoker invoker_compat_common invoker_compat_jni invoker_flowctl_pipectl invoker_flowctl_game)
 repo=git://github.com/sarum9in
 
+invoker_compat_jni_version="1.0.0-BUILD-SNAPSHOT"
+
 MFLAGS=()
 CMAKE_FLAGS=("-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.7" "-DCMAKE_C_COMPILER=/usr/bin/gcc-4.7")
 
