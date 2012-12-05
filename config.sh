a3fcd7e386d59243103f1d239219d149b92cb8ce
Filippov Aleksey <sarum9in@gmail.com>
2012-12-06 02:47:21 +0400

    Integration with bunsan::common.

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 lib_prefix=yandex_contest_
+extlibs=(bunsan_common)
 libs=(common system invoker invoker_compat_common invoker_flowctl_game invoker_compat_jni invoker_flowctl_pipectl)
 repo=git://github.com/sarum9in
+
+alllibs=("${extlibs[@]}" "${libs[@]}")
 
 invoker_compat_jni_version="1.0.0-BUILD-SNAPSHOT"
 
