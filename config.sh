de48fbe6abe12fdcd904a4cebbec663c6964d18b
Aleksey Filippov <sarum9in@gmail.com>
2013-09-09 15:29:01 +0400

    Integration with libraries' update.

--- config.sh
+++ config.sh
@@ -1,14 +1,24 @@
+extlib_prefix=bunsan_
 lib_prefix=yandex_contest_
-extlibs=(bunsan_common)
+extlibs=(cmake testing common)
 libs=(common system invoker invoker_compat_common invoker_flowctl_game invoker_compat_jni invoker_flowctl_pipectl)
 repo=git://github.com/sarum9in
 
-alllibs=("${extlibs[@]}" "${libs[@]}")
+
+alllibs=()
+for i in "${extlibs[@]}"
+do
+    alllibs+=("$extlib_prefix$i")
+done
+for i in "${libs[@]}"
+do
+    alllibs+=("$lib_prefix$i")
+done
 
 invoker_compat_jni_version="1.0.0-BUILD-SNAPSHOT"
 
 MFLAGS=()
-CMAKE_FLAGS=("-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.7" "-DCMAKE_C_COMPILER=/usr/bin/gcc-4.7")
+CMAKE_FLAGS=("-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8" "-DCMAKE_C_COMPILER=/usr/bin/gcc-4.8")
 
 if [[ -f user-config.sh ]]
 then
